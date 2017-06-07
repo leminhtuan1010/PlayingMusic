6e07ebd5c0a951c42c53a88ce3ac83e07165584e
leminhtuan1010 <leminhtuan1010@gmail.com>
2017-06-07 11:37:16 +0700

    first commit

--- PlayingMusic/ViewController.swift
+++ PlayingMusic/ViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn_Play: UIButton!
+    
+    @IBOutlet weak var sld_volume: UISlider!
+    var audio = AVAudioPlayer()
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        audio = try!AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: Bundle.main.path(forResource: "music",ofType: ".mp3")!) as URL)
+        audio.prepareToPlay()
+        addThumbImgForSlider()
+    }
+    func addThumbImgForSlider(){
+        sld_volume.setThumbImage(UIImage(named: "thumb.png"), for: .normal)
+        sld_volume.setThumbImage(UIImage(named: "thumbHightLight.png"), for: .normal)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func acc_Play(_ sender: Any) {
+        if (count == 0){
+            audio.play()
+            btn_Play.setImage(UIImage(named: "pause"), for: .normal)
+            count = 1
+        }else if (count == 1){
+            audio.stop()
+            btn_Play.setImage(UIImage(named: "play"), for: .normal)
+            count = 0
+        }
+        
+    }
+    @IBAction func sld_volume(_ sender: UISlider) {
+        audio.volume = sender.value
     }
 
 
